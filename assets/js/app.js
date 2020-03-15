bed720d5a36eb99f459471695f6e6203d693d3ff
mike-briggs <michael.briggs@queensu.ca>
2020-03-15 11:08:32 -0400

    added particles.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
 {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 160,
       "density": {
         "enable": true,
         "value_area": 800
@@ -34,11 +34,7 @@ particlesJS('particles-js',
       "polygon": {
         "nb_sides": 5
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+      
     },
     "opacity": {
       "value": 1,
