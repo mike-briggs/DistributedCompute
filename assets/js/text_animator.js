9915b42695a3420318100bed6c2975e50119832e
And1210 <amf7crazy@gmail.com>
2020-03-15 17:55:42 -0400

    Finished text animation and section involved with it

---
+++ assets/js/text_animator.js
@@ -0,0 +1,62 @@
+//Constants
+let MAX_TYPE = 0.25; //Min and max random type speed
+let MIN_TYPE = 0.05;
+
+let options = ["Test0", "Artificial Intelligence", "Test1", "Hello World!"]; //Text options
+
+//Variables
+let curOption = -1;
+let	charIndex = 0;
+let lastAnim = getTime();
+let resumeTime = 0;
+let t;
+let interval = null;
+let flag = true;
+
+let text = document.getElementById("to_animate_0");
+
+setInterval(checkUpdate, 10);
+
+function checkUpdate() {
+	if (flag == true){
+		changeText();
+		flag = false;
+	}
+}
+
+function changeText() {
+	if (interval != null)
+		clearInterval(interval);
+	curOption++;
+	curOption = curOption % options.length;
+	charIndex = 0;
+	setupAnimation();
+}
+
+function setupAnimation() {
+		t = Math.random() * (MAX_TYPE - MIN_TYPE) + MIN_TYPE;
+		interval = setInterval(animateText, 1);
+}
+
+function animateText() {
+	if (charIndex < options[curOption].length) {
+		if (getTime() > lastAnim + t) {
+			t = Math.random() * (MAX_TYPE - MIN_TYPE) + MIN_TYPE;
+			lastAnim = getTime();
+			charIndex++;
+		}
+	} else {
+		if (getTime() > lastAnim + 1) {
+			flag = true;
+		}
+	}
+	setText(options[curOption].substring(0, charIndex));
+}
+
+function setText(str_in) {
+	text.innerHTML = "<b>>"+str_in+"_</b>";
+}
+
+function getTime() { //Returns the time since 1970 in seconds
+	return Date.now()/1000;
+}
