bfe2e55484aa9af74b6a04e4734ff5c5fa8c3e1d
And1210 <amf7crazy@gmail.com>
2020-03-20 20:24:16 -0400

    Improved the header animation

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,10 @@
 let header = $("#header")[0];
 let lastScrollTop = 10;
 
+let lastScroll = Date.now()/1000;
+
+let moving = false;
+
 // $(document).ready(function() {
 // 	// header.style.cssText = "display:none;";
 // });
@@ -8,13 +12,23 @@ let lastScrollTop = 10;
 $(document).scroll(function(event) {
 	let st = $(this).scrollTop();
 
-	if (st > lastScrollTop) {
-		//Scroll down
-		$(header).fadeTo(20, 0);
-	} else {
-		//Scroll up
-		$(header).fadeTo(1, 1);
+	if (moving == false) {
+		if (st > lastScrollTop) {
+			//Scroll down
+			$(header).fadeTo(20, 0);
+		} else {
+			//Scroll up
+			$(header).fadeTo(1, 1);
+		}
+		lastScroll = getTime();
+		moving = true;
+	} else if (getTime() - lastScroll > 20/1000) {
+		moving = false;
 	}
 
 	lastScrollTop = st;
 });
+
+function getTime() { //Returns the time since 1970 in seconds
+	return Date.now()/1000;
+}
